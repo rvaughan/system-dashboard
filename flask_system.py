9fe6fa09542f0a58c6cdf4097af03f398ae0142e
Tomas Trnka <trnkat@avast.com>
2017-07-04 21:47:06 +0200

    working on it

--- flask_system.py
+++ flask_system.py
@@ -4,6 +4,9 @@ import os
 import time
 
 import psutil
+import xmltodict
+import subprocess
+
 from flask import Flask
 from flask_cors import CORS, cross_origin
 
@@ -19,7 +22,7 @@ log.setLevel(logging.ERROR)
 
 
 @cross_origin()
-@app.route('/')
+@app.route('/cpu')
 def index():
 
     disk_write_data_start = psutil.disk_io_counters(perdisk=False)
@@ -62,7 +65,50 @@ def index():
 
     return json.dumps(data)
 
+@cross_origin()
+@app.route('/processes')
+def processes():
+
+    time.sleep(period)
+    
+    gh = subprocess.Popen(["ps", "aux"], stdout=subprocess.PIPE)
+    ll = gh.stdout.read().decode("utf-8")
+    processes = [line.split()[:2] for line in ll.strip().split("\n")]
+    process_user = {b:a for a,b in  processes}
+    return json.dumps({"data":process_user})
+
+
+@cross_origin()
+@app.route('/nvidia-smi')
+def nvidia():
+
+    time.sleep(period)
+    # TODO
+    """ 
+    nvidia_smi = subprocess.Popen(["nvidia-smi", "-q", "-x"], stdout=subprocess.PIPE)
+    nv_dict = xmltodict.parse(nvidia_smi.stdout.read())
+    """
+    nv_dict = xmltodict.parse(open("/home/trnkat/Documents/nv.txt").read())
+    gpus = []
+    if 'nvidia_smi_log' in nv_dict:
+        log = nv_dict["nvidia_smi_log"]
+        for gpu in log["gpu"]:
+            fields = ["processes", "@id", "fan_speed", "utilization",
+                      "temperature", "clocks"]
+            gpu_dict = {f:gpu[f] for f in fields}
+            gpus.append(gpu_dict)            
+
+    #cpu_data = psutil.cpu_percent(interval=None)
+    #ram_data = psutil.virtual_memory()
+    #disk_data = psutil.disk_usage('/')
+    #disk_write_data = psutil.disk_io_counters(perdisk=False)
+    #io_data = psutil.net_io_counters()
+
+    return json.dumps({"data":gpus})
+
+
+
 if __name__ == "__main__":
     print("System API up at http://localhost:5002")
     PORT = int(os.getenv('PORT', 5002))
-    app.run(port=PORT, threaded=True)
+    app.run(port=PORT, threaded=True, debug=True)
